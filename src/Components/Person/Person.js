d81fb59f3fc2a64b0bb04b9cd819ded647bcdcf3
soma sekhar novudu <novudu.somasekhar702@gmail.com>
2018-02-28 22:56:38 +0530

    test commit

--- src/Components/Person/Person.js
+++ src/Components/Person/Person.js
@@ -1,12 +1,14 @@
 import React , {Component} from 'react';
 import './Person.css';
 
+
 const Person=(props)=>{
 
     return(
         <div className="Person">
            <p>I am {props.name} and i am {props.age}</p>
            <p>{props.children}</p>
+           <p>test word</p>
         </div>
     )
 }
